c6c08d95f53d11347ccf0c067c65fc6fc9ff645b
Harsh <valambeharsh1701@gmail.com>
2023-02-03 00:53:48 +0530

    challenge2 completed

---
+++ work1.js
@@ -0,0 +1,11 @@
+// const os=require("os");
+// console.log(os.arch());
+// console.log(os.hostname());
+// const freeMemory=os.freemem();
+// console.log(`${freeMemory/1024/1024/1024}`);
+
+
+
+
+
+
