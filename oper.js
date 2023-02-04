56adef9dd48c2e911ab46ca8989af73041fcb13f
Harsh <valambeharsh1701@gmail.com>
2023-02-04 20:50:05 +0530

    challenge2 completed

---
+++ oper.js
@@ -0,0 +1,9 @@
+
+
+
+const add=(a,b)=>{
+    return a+b;
+}
+
+module.exports=add;
+
--- work1.js
+++ work1.js
@@ -4,7 +4,8 @@
 // const freeMemory=os.freemem();
 // console.log(`${freeMemory/1024/1024/1024}`);
 
-
+const add=require("./oper");
+console.log(add(5,3));
 
 
 
